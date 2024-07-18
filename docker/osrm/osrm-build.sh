8b8572b759d5304bc70f1a0e9379f050f14db50e
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-07-18 13:28:47 +0200

    Allow to merge pbf before OSRM build

--- docker/osrm/osrm-build.sh
+++ docker/osrm/osrm-build.sh
@@ -8,26 +8,37 @@ die() {
 }
 
 [ -z "${BASENAME}" ] && die "BASENAME environment variable must be provided."
-[ -z "${PBF_URL}" ] && die "PBF_URL environment variable must be provided."
+[ -z "${PBF_URLS}" ] && die "PBF_URLS environment variable must be provided."
 [ -z "${PROFILE}" ] && die "PROFILE environment variable must be provided."
 
 DATE=$(date +%Y%m%d)
-PBF_LATEST=$(basename "$PBF_URL")
-PBF_DATE=${PBF_LATEST/latest/$DATE}
 
-if [[ -s "/srv/osrm/data/${PBF_DATE}" ]]; then
-    echo "PBF exists, skip download: ${PBF_DATE}"
-else
-    echo "Dowload PBF ${PBF_DATE}"
-    curl ${PBF_URL} > /srv/osrm/data/${PBF_DATE}
-    rm -fr /srv/osrm/data/${PBF_LATEST}
-    ln -s ${PBF_DATE} /srv/osrm/data/${PBF_LATEST}
-fi
+PBF_LATESTS=""
+for PBF_URL in $PBF_URLS; do
+    PBF_LATEST=$(basename "$PBF_URL")
+    PBF_DATE=${PBF_LATEST/latest/$DATE}
+
+    if [[ -s "/srv/osrm/data/${PBF_DATE}" ]]; then
+        echo "PBF exists, skip download: ${PBF_DATE}"
+    else
+        echo "Dowload PBF ${PBF_DATE}"
+        curl ${PBF_URL} > /srv/osrm/data/${PBF_DATE}
+        rm -fr /srv/osrm/data/${PBF_LATEST}
+        ln -s ${PBF_DATE} /srv/osrm/data/${PBF_LATEST}
+    fi
+
+    PBF_LATESTS="${PBF_LATESTS} /srv/osrm/data/${PBF_DATE}"
+done
 
 BASENAME_PBF_LATEST=${BASENAME}-latest.osm.pbf
 BASENAME_PBF_DATE=${BASENAME}-${DATE}.osm.pbf
 rm -fr /srv/osrm/data/${BASENAME_PBF_DATE}
-ln -s ${PBF_DATE} /srv/osrm/data/${BASENAME_PBF_DATE}
+
+if [[ ${PBF_LATESTS:1} =~ " " ]]; then
+    osmium merge ${PBF_LATESTS} -o /srv/osrm/data/${BASENAME_PBF_DATE}
+else
+    ln -s $PBF_LATESTS /srv/osrm/data/${BASENAME_PBF_DATE}
+fi
 
 osrm-extract \
     --location-dependent-data /usr/local/share/osrm/data/driving_side.geojson \
