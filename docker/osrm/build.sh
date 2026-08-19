123eff97c60494da71bcd87a5d7492b6100b9903
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-08-19 15:56:17 +0200

    Add speed_adjustement_zone.geojson

--- docker/osrm/build.sh
+++ docker/osrm/build.sh
@@ -53,6 +53,7 @@ osrm-extract \
     --location-dependent-data /usr/local/share/osrm/data/maxheight.geojson \
     --location-dependent-data /usr/local/share/osrm/data/low_emission_zone.geojson \
     --location-dependent-data /usr/local/share/osrm/data/limited_traffic_zone.geojson \
+    --location-dependent-data /usr/local/share/osrm/data/speed_adjustement_zone.geojson \
     --with-osm-metadata \
     -p ${PROFILE} \
     /srv/osrm/data/${BASENAME_PBF_DATE}
